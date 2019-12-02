6eb565c63abfbcb3c162f74369a444c88a161c55
Jackson Pereira da Silva <jacksonga@hotmail.com>
2019-12-02 02:39:59 -0300

    Subindo projeto inicial

---
+++ demo/src/test/java/com/jackson/casadocodigo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
